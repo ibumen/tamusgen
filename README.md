45fb92930942b99e093b73b6784a3ce137b06e25
Umar Ibrahim Enesi <contactenesi@gmail.com>
2016-04-12 23:42:42 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+tamusgen
+========
+
+A Symfony project created on April 11, 2016, 11:58 pm.
---
+++ src/AppBundle/Controller/AssetController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class AssetController extends Controller {
+
+    /**
+     * @Route("/css/{css}")
+     */
+    public function aAction($css, Request $request) {
+        $host = $request->getHost();
+        //echo $host;
+        return $this->redirect("http://" . $host . "/tamusgen/web/css/$css");
+    }
+
+    /**
+     * @Route("/images/{img}")
+     */
+    public function bAction($img) {
+        $host = $request->getHost();
+        //echo $host;
+        return $this->redirect("http://" . $host . "/tamusgen/web/images/$img");
+    }
+    
+    /**
+     * @Route("/js/{js}")
+     */
+    public function cAction($js) {
+        $host = $request->getHost();
+        //echo $host;
+        return $this->redirect("http://" . $host . "/tamusgen/web/js/$img");
+    }
+
+}
