fa1db19506b22561f5633b5fec34983107d3ef99
Umar Ibrahim Enesi <contactenesi@gmail.com>
2016-04-17 08:32:54 +0100

    modified the index page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -18,5 +19,19 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
+    }
+    
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction(Request $request){
+        return $this->redirectToRoute("homepage");
+    }
+    
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function adminAction(Request $request){
+        return new Response("Admin");
     }
 }
